f56a5df282da1ebef6fcced3d0ab3ee990391e83
Lolrelic12 <44398441+Lolrelic12@users.noreply.github.com>
2024-01-29 23:31:56 +0700

    added debug options and fixed pump controls

--- auto_feeder.ino
+++ auto_feeder.ino
@@ -4,11 +4,10 @@
 // group: group 9
 // author: phathnhe187251
 // date created: jan 28, 2024
-// last modified:  15:23 jan 29, 2024
+// last modified:  23:31 jan 29, 2024
 // license: creative commons attribution non commercial share alike (cc by-nc-sa 3.0)
 
 // name: auto feeder
-// version: v0.8.2-beta
 // description: a machine that detects when food and water has run out and automatically attempts to refill
 // intelligently only carries out refills when it is safe to do so
 // perfect for use in cattle and poultry farming
@@ -21,41 +20,51 @@
 // bowl state detector: generic photoresistor
 // water level sensor: sen18 water level sensor
 // water pump: mini submersible auto-priming pump
+// relay switch: jqc-3ff-s-z 5v 12a relay
 
 // notes:
 // tune values before running to ensure correct operation
-// uncomment line 58 to enable serial debugging
 
 
 #include <Servo.h>
 
 
+const String version = "v0.9.6r3-beta";
+const bool serialDebug = false;  // set to true to enable serial debugging
+const int baudRate = 9600;
+
 Servo feederBox;
 const int feederBoxPin = 8;
 const int close = 75, open = 20;
 
-const int pumpPin = 7;
+// pump is connected to external power source and controlled by a relay switch
+// this particular switch is low-active, so controls are inverted
+const int pumpRelayPin = 7;  
 
 // ultrasonic presence detector calibrations
 const int trigPin = 3, echoPin = 4;
 bool presence = true;
-const float distanceThreshold = 10; // change to configure how close to the sensor is considered present, in cm
+const float distanceThreshold = 10;  // change to configure how close to the sensor is considered present, in cm
 
 // photoresistor calibrations
 const int bowlBottom = A0, bowlTop = A1;
 bool bowlFull = true, bowlEmpty = false;
-int photoresMin = 1, photoresMax = 860; // tune these values before running (measured with 330 ohm resistors)
-int photoresThreshold = (photoresMin + photoresMax) / 60;
+int photoresMin = 1, photoresMax = 860;        // tune these values before running (measured with 330 ohm resistors)
+const float photoresThresholdMultiplier = 0.02; // value from 0 to 1, values closer to 1 means higher threshold
+int photoresThreshold = (photoresMin + photoresMax) / (1 / photoresThresholdMultiplier);
 
 // water sensor calibrations
 const int waterPowerPin = 6, waterReadPin = A2;
-const int waterEmpty = 0, waterFull = 280; // tune these values before running
-const int waterThreshold = (waterEmpty + waterFull) / 2; // how low is considered low, default is 1/4
+const int waterEmpty = 0, waterFull = 450;    // tune these values before running
+const float waterThresholdMultiplier = 0.95;  // value from 0 to 1, values closer to 1 means higher threshold
+const int waterThreshold = (waterEmpty + waterFull) / (1 / waterThresholdMultiplier);
 int waterLevel = waterFull;
 
 
 void setup() {
-  // Serial.begin(9600);
+  if (serialDebug == true) {
+    Serial.begin(baudRate);
+  }
 
   pinMode(bowlBottom, INPUT);
   pinMode(bowlTop, INPUT);
@@ -67,10 +76,10 @@ void setup() {
   pinMode(waterReadPin, INPUT);
 
   feederBox.attach(feederBoxPin);
-  feederBox.write(close); // ensure feeder box is closed and  door secured
+  feederBox.write(close);  // ensures feeder box is closed and  door secured
 
-  pinMode(pumpPin, OUTPUT);
-  digitalWrite(pumpPin, HIGH); // pump is connected to external power source, grounded to this pin, controls are inverted
+  pinMode(pumpRelayPin, OUTPUT);
+  digitalWrite(pumpRelayPin, HIGH); // ensures pump is off
 }
 
 void loop() {
@@ -80,25 +89,27 @@ void loop() {
   waterLevel = measureWater();
   presence = detectPresence();
 
-  // Serial.println(presence);
+  if (serialDebug == true) {
+    printDebug();
+  }
 
   // feeder box is open only when bowl is empty and it is not full and no presence is detected
-  if (bowlEmpty && !bowlFull && !presence) { 
-    feederBox.write(open); 
+  if (bowlEmpty && !bowlFull && !presence) {
+    feederBox.write(open);
   }
   // if the bowl becomes full or presence is detected, refilling stops
-  if (bowlFull || presence) { 
-    feederBox.write(close); 
+  if (bowlFull || presence) {
+    feederBox.write(close);
   }
 
   // pump water in when the water level goes below the threshold
-  if (waterLevel <= waterThreshold ) {
-    digitalWrite(pumpPin, LOW);
+  if (waterLevel <= waterThreshold) {
+    digitalWrite(pumpRelayPin, LOW);
   }
   // if the water bowl is full, pumping stops
   if (waterLevel >= waterFull) {
-    digitalWrite(pumpPin, HIGH);
-  } 
+    digitalWrite(pumpRelayPin, HIGH);
+  }
 
   delay(250);
 }
@@ -140,9 +151,56 @@ bool detectPresence() {
 // measures water using water level sensor
 // returns an integer signifying the water level in the container
 int measureWater() {
-	digitalWrite(waterPowerPin, HIGH);
-	delay(10);
-	int val = analogRead(waterReadPin);
-	digitalWrite(waterPowerPin, LOW);
-	return val;
+  digitalWrite(waterPowerPin, HIGH);
+  delay(10);
+  int val = analogRead(waterReadPin);
+  digitalWrite(waterPowerPin, LOW);
+  return val;
+}
+
+// for development use only
+// prints debug info to the serial monitor
+void printDebug() {
+  Serial.print("FarmWare ");
+  Serial.print(version);
+  Serial.println(" serial debugger\n");
+
+  Serial.print("Presence detection: ");
+  if (presence == true) {
+    Serial.println("detected");
+  } else {
+    Serial.println("none");
+  }
+  Serial.print("Detection range: ");
+  Serial.print((int)distanceThreshold);
+  Serial.println("cm\n");
+
+  Serial.print("Bowl state: ");
+  if (presence == true) {
+    Serial.println("presence detected");
+    Serial.println("Feeder box: closed\n");
+  } else {
+    if (bowlFull) {
+      Serial.println("full");
+      Serial.println("Feeder box: closed\n");
+    } else if (bowlEmpty) {
+      Serial.println("empty");
+      Serial.println("Feeder box: open\n");
+    } else {
+      Serial.println("nominal");
+      Serial.println("Feeder box: closed\n");
+    }
+  }
+
+  Serial.print("Water level: ");
+  Serial.println(waterLevel);
+  Serial.print("Water level threshold: ");
+  Serial.println(waterThreshold);
+  if (waterLevel <= waterThreshold) {
+    Serial.println("Pump: running\n");
+  } else {
+    Serial.println("Pump: standby\n");
+  }
+
+  delay(750);
 }
