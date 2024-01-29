1f18556960fda7813e6f66c394b109d3e8cf6a9c
Lolrelic12 <44398441+Lolrelic12@users.noreply.github.com>
2024-01-29 14:44:20 +0700

    calibrated parameters

--- auto_feeder.ino
+++ auto_feeder.ino
@@ -4,11 +4,11 @@
 // group: group 9
 // author: phathnhe187251
 // date created: jan 28, 2024
-// last modified:  00:14 jan 29, 2024
+// last modified:  14:143 jan 29, 2024
 // license: creative commons attribution non commercial share alike (cc by-nc-sa 3.0)
 
 // name: auto feeder
-// version: v0.7.4-beta
+// version: v0.8.1-beta
 // description: a machine that detects when food and water has run out and automatically attempts to refill
 // intelligently only carries out refills when it is safe to do so
 // perfect for use in cattle and poultry farming
@@ -32,30 +32,30 @@
 
 Servo feederBox;
 const int feederBoxPin = 8;
-const int close = 0, open = 45;
+const int close = 75, open = 20;
 
 const int pumpPin = 7;
 
 // ultrasonic presence detector calibrations
 const int trigPin = 3, echoPin = 4;
 bool presence = true;
-const float distanceThreshold = 50; // change to configure how close to the sensor is considered present, in cm
+const float distanceThreshold = 10; // change to configure how close to the sensor is considered present, in cm
 
 // photoresistor calibrations
 const int bowlBottom = A0, bowlTop = A1;
 bool bowlFull = true, bowlEmpty = false;
-int photoresMin = 1, photoresMax = 310; // tune these values before running
-int photoresThreshold = (photoresMin + photoresMax) / 2;
+int photoresMin = 1, photoresMax = 860; // tune these values before running (measured with 330 ohm resistors)
+int photoresThreshold = (photoresMin + photoresMax) / 60;
 
 // water sensor calibrations
-const int waterPowerPin = 6, waterReadPin = A3;
-const int waterEmpty = 0, waterFull = 500; // tune these values before running
-const int waterThreshold = waterEmpty + ((waterEmpty + waterFull) / 4); // how low is considered low, default is 1/4
+const int waterPowerPin = 6, waterReadPin = A2;
+const int waterEmpty = 0, waterFull = 280; // tune these values before running
+const int waterThreshold = (waterEmpty + waterFull) / 2; // how low is considered low, default is 1/4
 int waterLevel = waterFull;
 
 
 void setup() {
-  // Serial.begin(9600);
+  Serial.begin(9600);
 
   pinMode(bowlBottom, INPUT);
   pinMode(bowlTop, INPUT);
@@ -80,6 +80,8 @@ void loop() {
   waterLevel = measureWater();
   presence = detectPresence();
 
+  Serial.println(waterLevel);
+
   // feeder box is open only when bowl is empty and it is not full and no presence is detected
   if (bowlEmpty && !bowlFull && !presence) { 
     feederBox.write(open); 
@@ -90,7 +92,7 @@ void loop() {
   }
 
   // pump water in when the water level goes below the threshold
-  if (waterLevel <= waterThreshold && waterLevel < waterFull) {
+  if (waterLevel <= waterThreshold ) {
     digitalWrite(pumpPin, LOW);
   }
   // if the water bowl is full, pumping stops
